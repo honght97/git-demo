d5d6166ffd6212b95f331377e1a8b5f66f9276d3
honght97 <hong.ht150297@gmail.com>
2021-09-18 16:43:54 +0700

    add method die for Cat

--- cat.js
+++ cat.js
@@ -1,7 +1,11 @@
 function Cat() {
     this.stomach = [];
+    this.dead = false;
 }
 Cat.prototype.eat = function(mouse) {
     this.stomach.push(mouse);
+}
+Cat.prototype.die = function() {
+    this.dead = true;
 }
 module.exports = Cat;
--- dog.js
+++ dog.js
@@ -1,4 +1,7 @@
-function Dog() {
-
+function Dog(name) {
+    this.name = name;
+}
+Dog.prototype.eat  = function(cat) {
+    this.stomach.push(cat);
 }
 module.exports = Dog;
