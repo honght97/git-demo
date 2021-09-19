9600f9affb6aa4f12acf555fcb80c1993788a069
honght97 <hong.ht150297@gmail.com>
2021-09-19 16:13:54 +0700

    Let Dog eat Cat

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const Dog = require('./Dog');
+const Cat = require('./Cat');
+
 const dog = new Dog('Tom');
+const tom = new Cat('Tom');
+dog.eat(tom);
+console.log(dog);
+tom.die();
+console.log(tom);
 dog.sayHi();
